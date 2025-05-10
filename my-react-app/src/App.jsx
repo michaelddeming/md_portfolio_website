5d3c3b41db5cd7b17867c37e12f3c4ca14c28262
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-05-09 21:59:33 -0700

    Refactored Job/Position into WorkExperience.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,12 +2,33 @@ import Header from "./Header"
 import WorkExperience from "./WorkExperience"
 import Education from "./Education";
 
+
 function App() {
   return(
     <>
       <Header></Header>
-      <WorkExperience></WorkExperience>
-      <Education></Education>
+      <div className="index-content-main">
+
+        <div className="index-content-left">
+        
+        <WorkExperience
+        company_name="ThermoFisher Scientific"
+        job_title="Scientist II, Formulation Chemistry"
+        job_start="Jan. 2024"
+        job_end="Current"
+        job_highlight_1="test"
+        job_highlight_2="test"
+        job_highlight_3="test"
+        ></WorkExperience>
+        <Education></Education>
+        
+        </div>
+
+        
+        <div className="index-content-right">
+       
+        </div>
+      </div>
     </>
 
   );
--- my-react-app/src/WorkExperience.jsx
+++ my-react-app/src/WorkExperience.jsx
@@ -1,14 +1,28 @@
-import Job from "./Job"
 
-function WorkExperience(){
+
+function WorkExperience(props){
 
     return(<>
     <h2>EMPLOYMENT EXPERIENCE</h2>
-        <Job 
-        company_name="ThermoFisher Scientific"
-        ></Job>
+         <div className="job">
+            <h3>{props.company_name}</h3>
+                <div className="Position">
+                    <p className="job-title"><b>{props.job_title}</b>, <i>{props.job_start} - {props.job_end}</i></p>
+                    <ul>
+                        <li>
+                            {props.job_highlight_1}
+                        </li>
+                        <li>
+                            {props.job_highlight_2}
+                        </li>
+                        <li>
+                            {props.job_highlight_3}
+                        </li>
+                    </ul>
+                </div>
+            
+         </div>
     </>
-        
     );
 }
 
