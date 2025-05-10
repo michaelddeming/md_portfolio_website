23a451f12d17fd2216e7efb22b825198990b20b5
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-05-09 22:33:44 -0700

    Refactored Degree component as child of Education.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./Header"
 import WorkExperience from "./WorkExperience"
 import Education from "./Education";
 import Job from "./Job";
+import Degree from "./Degree";
 
 
 function App() {
@@ -11,7 +12,17 @@ function App() {
       <div className="index-content-main">
 
         <div className="index-content-left">
+        
         <WorkExperience>
+          <Job
+          company_name="Birdie Rose Clothes"
+          job_title="Software Engineer, Contractor"
+          job_start="Nov. 2024"
+          job_end="April 2025"
+          job_highlight_1="test"
+          job_highlight_2="test"
+          job_highlight_3="test"/>
+      
           <Job
           company_name="ThermoFisher Scientific"
           job_title="Scientist II, Formulation Chemistry"
@@ -20,11 +31,21 @@ function App() {
           job_highlight_1="test"
           job_highlight_2="test"
           job_highlight_3="test"
-          >
-          </Job>
+          />
+     
         </WorkExperience>
-        <Education></Education>
-        
+        <Education>
+          <Degree
+          degree_title="Bachelor of Science"
+          degree_major="Human Biology w/ Minor in Chemistry" 
+          institution_name="Utah State University"
+          institution_city="Logan"
+          institution_state="Utah"
+          graduation_month="December"
+          graduation_year="2021"
+          degree_highlight_desc="Magna Cum Laude"
+        />
+        </Education>
         </div>
 
         
--- my-react-app/src/Education.jsx
+++ my-react-app/src/Education.jsx
@@ -1,22 +1,10 @@
 import Degree from "./Degree"
 
-function Education(){
+function Education(props){
 
     return(<>
     <h2>EDUCATION</h2>
-        <Degree
-        degree_title="Bachelor of Science"
-        degree_major="Human Biology w/ Minor in Chemistry" 
-        institution_name="Utah State University"
-        institution_city="Logan"
-        institution_state="Utah"
-        graduation_month="December"
-        graduation_year="2021"
-        degree_highlight_title="Honors"
-        degree_highlight_desc="Magna Cum Laude"
-        ></Degree>
-        
-
+    <div>{props.children}</div>
     </>
         
     );
